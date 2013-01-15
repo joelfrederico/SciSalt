177f4b6a06126a1299b2dbbe205441cd4fd1287d
Joel Frederico <joelfred@slac.stanford.edu>
2013-01-15 11:07:18 -0800

    Updated to include the ability to use a currently-open figure.

--- hist2d.py
+++ hist2d.py
@@ -2,14 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from showfig import showfig
 
-def hist2d(x,y,bins=10,aspect="auto",plot=True,range=None):
+def hist2d(x, y, bins=10, aspect="auto", plot=True, fig=None, range=None):
 	"""Creates a 2D histogram of data."""
 
 	h,xe,ye=np.histogram2d(x,y,bins=bins,range=range)
 	extent=[xe[0],xe[-1],ye[-1],ye[0]]
 	# fig=plt.figure()
 	if plot:
-		fig=plt.gcf()
+		if ( fig==None ):
+			fig=plt.gcf()
 		ax=fig.gca()
 		ax.clear()
 		img=ax.imshow(h.transpose(),extent=extent,interpolation="none",aspect=aspect)
