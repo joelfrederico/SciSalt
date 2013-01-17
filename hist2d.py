072ff610d09e565dc47812168af99141d62f1f15
Joel Frederico <joelfred@slac.stanford.edu>
2013-01-16 16:30:27 -0800

    Added labels to hist2d.

--- hist2d.py
+++ hist2d.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from showfig import showfig
+from addlabel import addlabel
 
-def hist2d(x, y, bins=10, aspect="auto", plot=True, fig=None, range=None):
+def hist2d(x, y, bins=10, labels=None, aspect="auto", plot=True, fig=None, range=None):
 	"""Creates a 2D histogram of data."""
 
 	h,xe,ye=np.histogram2d(x,y,bins=bins,range=range)
@@ -15,6 +16,9 @@ def hist2d(x, y, bins=10, aspect="auto", plot=True, fig=None, range=None):
 		ax.clear()
 		img=ax.imshow(h.transpose(),extent=extent,interpolation="none",aspect=aspect)
 		plt.colorbar(img)
+		if ( labels != None ):
+			addlabel(labels[0],labels[1],labels[2])
+
 		showfig(fig,aspect)
 	return h,extent
 
