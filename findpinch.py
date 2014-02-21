e9491f9193926e9b753d6fb3f1c1064d75fea5ed
Joel Frederico <joelfred@slac.stanford.edu>
2014-02-20 21:34:16 -0800

    Reworked so that image is rotated properly in code.

--- findpinch.py
+++ findpinch.py
@@ -3,9 +3,12 @@ import numpy as _np
 import matplotlib.pyplot as _plt
 import mytools as _mt
 # import copy
+import pdb as _pdb
 
-def findpinch(img,xbounds=None,ybounds=None,step=1):
+def findpinch(img,xbounds=None,ybounds=None,step=1,verbose=False):
+	# ======================================
 	# Translate to clearer variables
+	# ======================================
 	xstart = xbounds[0]
 	xstop  = xbounds[1]
 	ystart = ybounds[0]
@@ -14,58 +17,70 @@ def findpinch(img,xbounds=None,ybounds=None,step=1):
 	xrange = slice(xstart,xstop)
 	yrange = slice(ystart,ystop)
 
-	fig=_mt.figure('To process')
-	# a=img
-	_plt.imshow(img[xrange,yrange])
+	img=img[yrange,xrange]
+	if verbose:
+		fig=_mt.figure('To process')
+		_plt.imshow(img)
+		_plt.show()
 	
-	# Check number of points and initialize arrays
-	num_pts=(xstop-xstart)/step
-	sigs=_np.zeros(num_pts)
-	# num_pts = len(sum_x)
+	# ======================================
+	# Check number of points and
+	# initialize arrays
+	# ======================================
+	num_pts   = (ystop-ystart)/step
+	sigs      = _np.zeros(num_pts)
 	variance  = _np.zeros(num_pts)
 	stddev    = _np.zeros(num_pts)
 	varerr    = _np.zeros(num_pts)
-	# print varerr.shape
 	chisq_red = _np.zeros(num_pts)
 	
+	# ======================================
 	# Fit individual slices
-	for i,val in enumerate(_mt.linspacestep(xstart,xstop-step,step)):
-		print i
+	# ======================================
+	for i,val in enumerate(_mt.linspacestep(0,ystop-ystart-step,step)):
 		# Take a strip of the image
-		# strip = img[slice(475,480),yrange]
-		strip = img[slice(val,val+step),yrange]
-		# fig=_mt.figure('Strip')
-		# _plt.imshow(strip)
+		strip = img[slice(val,val+step),:]
 		
 		# Sum over the strip to get an average of sorts
-		histdata = sum(strip)
+		histdata = _np.sum(strip,0)
 		xbins = len(histdata)
 		x = _np.linspace(1,xbins,xbins)
-		# _plt.plot(x,histdata)
+		if verbose:
+			_plt.plot(x,histdata,'.-')
+			_plt.show()
+			_pdb.set_trace()
 		
 		# Fit with a Gaussian to find spot size
-		# fig=_mt.figure('Gaussian Fit')
 		# plotbool=True
 		plotbool = False
 		varbool  = False
-		# popt,pcov,chisq_red = _mt.gaussfit(x,histdata,sigma_y=_np.ones(xbins),plot=plotbool,variance_bool=varbool,verbose=False,p0=[16000,3500,500,0])
-		popt,pcov,chisq_red = _mt.gaussfit(x,histdata,sigma_y=_np.ones(xbins),plot=plotbool,variance_bool=varbool,verbose=False)
+		popt,pcov,chisq_red[i] = _mt.gaussfit(
+				x,
+				histdata,
+				sigma_y=_np.ones(xbins),
+				plot=plotbool,
+				variance_bool=varbool,
+				background_bool=True,
+				verbose=False)
 	
 		variance[i] = popt[2]
-		# varerr[i]   = pcov[2,2]
-		# stddev[i]   = _np.sqrt(pcov[2,2])
 	
-	_mt.figure('Varplot')
-	# _plt.plot(variance)
+	# ======================================
+	# Fit 2nd-deg poly to results
+	# ======================================
+	yvar=_np.shape(_mt.linspacestep(ystart,ystop,step))[0]-1
+	yvar=_mt.linspacestep(1,yvar)
 	
-	xvar=_np.shape(_mt.linspacestep(xstart,xstop,step))[0]-1
-	xvar=_mt.linspacestep(1,xvar)
-	
-	out=_np.polyfit(xvar,variance,2)
-	
-	_plt.plot(xvar,variance,'.-',xvar,_np.polyval(out,xvar))
+	out=_np.polyfit(yvar,variance,2)
 
+	if verbose:
+		_plt.plot(yvar,variance,yvar,_np.polyval(out,yvar))
+		_plt.show()
+
+	# ======================================
+	# Report minimum in steps and pixels
+	# ======================================
 	fitmin=-out[1]/(2*out[0])
-	pxmin = fitmin*step+xstart
+	pxmin = fitmin*step+ystart
 
 	print 'Minimum at {} step, {}px'.format(fitmin,pxmin)
