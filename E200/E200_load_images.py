b95dcaf527c949c0489822f116bf61accd41745c
Joel Frederico <joelfred@slac.stanford.edu>
2014-05-16 15:10:41 -0700

    Fixed E200_load_images to work with local files

--- E200/E200_load_images.py
+++ E200/E200_load_images.py
@@ -9,7 +9,13 @@ from get_remoteprefix import get_remoteprefix
 import os
 
 def E200_load_images(imgstr,h5file,uids=None):
-	prefix = get_remoteprefix()
+	remote_bool = h5file['data']['VersionInfo']['remotefiles']['dat'][0,0]
+	if remote_bool:
+		prefix = get_remoteprefix()
+	else:
+		prefix = ''
+	# print 'Prefix is: [{}]'.format(prefix)
+
 	imgdat = E200_api_getdat(imgstr,h5file)
 	imgs = [_plt.imread(os.path.join(prefix,val)) for val in imgdat]
 	imgs = _np.float64(imgs)
@@ -18,12 +24,13 @@ def E200_load_images(imgstr,h5file,uids=None):
 	# print imgbgdat
 	for i,val in enumerate(imgbgdat):
 		# print val
+		val = os.path.join(prefix,val)
 		mat = _spio.loadmat(val)
 		imgbg = mat['img']
 
 		if imgs[i,:,:].shape[0] == imgbg.shape[1]:
 			imgbg = _np.transpose(imgbg)
 
-		imgs[i,:,:] = _np.abs(imgs[i,:,:]-_np.float64(imgbg))
+		imgs[i,:,:] = _np.fliplr(_np.abs(imgs[i,:,:]-_np.float64(imgbg)))
 	
 	return _np.array(imgs)
