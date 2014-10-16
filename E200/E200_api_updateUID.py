2986b3603c5409b9090dc058025928129adf580e
Joel Frederico <joelfred@slac.stanford.edu>
2014-10-16 16:04:15 -0700

    Consolidated everything into one datatype that uses refs

--- E200/E200_api_updateUID.py
+++ E200/E200_api_updateUID.py
@@ -1,42 +1,13 @@
-import numpy as np
-import h5py as h5
+import numpy as _np
+import h5py as _h5
+import uuid as _uuid
 import warnings
 from scalar_update import *
 from vector_update import *
 from array_update import *
 
 def E200_api_updateUID(group,UID,value,verbose=False):
-	datatype = group.attrs['datatype']
-	if verbose: print '\tDatatype is {}'.format(datatype)
-
-	# ======================================
-	# Validate value, uid same size
-	# ======================================
-	if datatype == 'scalar':
-		if np.size(value) != np.size(UID):
-			raise ValueError('UIDs and values not commensurate sizes. UIDs: {}, Values: {}'.format(UID,value))
-
-	elif datatype == 'vector':
-		# ======================================
-		# Get right length of value
-		# ======================================
-		if np.ndim(value) == 1:
-			if np.size(UID) != 1:
-				raise ValueError('UIDs and values not commensurate sizes. UIDs: {}, Values: {}'.format(UID,value))
-
-		elif np.ndim(value) == 2:
-			if np.shape(value)[0] != np.size(UID):
-				raise ValueError('UIDs and values not commensurate sizes. UIDs: {}, Values: {}'.format(UID,value))
-		else:
-			raise ValueError('Number of dimensions for array of vectors incorrect: {} dimensions'.format(np.ndim(value)))
-
-	elif datatype == 'array':
-			raise NotImplementedError('Arrays not implemented yet')
-
-	else:
-		raise TypeError('This is not a group supported by the E200_api')
-
-	if np.size(UID)>1:
+	if _np.size(UID)>1:
 		# ======================================
 		# Run through list of UID's, processing
 		# individually
@@ -46,36 +17,90 @@ def E200_api_updateUID(group,UID,value,verbose=False):
 		# ======================================
 		# Check for duplicate UIDs and sort
 		# ======================================
-		UID_sorted,inds = np.unique(UID,return_index=True)
+		UID_sorted,inds = _np.unique(UID,return_index=True)
 		value_sorted    = value[inds]
-		if np.size(UID_sorted) != np.size(UID):
+		if _np.size(UID_sorted) != _np.size(UID):
 			raise ValueError('UIDs aren''t unique: {}'.format(UID))
 
 		for i,uid in enumerate(UID_sorted):
 			E200_api_updateUID(group,value=value_sorted[i],UID=uid,verbose=verbose)
 
 	else:
-		# ======================================
-		# Retrieve data type
-		# ======================================
-		if verbose: print 'Single UID entered: {}'.format(UID)
+	
+		ref_dtype = h5.special_dtype(ref=h5.Reference)
 	
 		# ======================================
-		# Scalar type
+		# Get basic groups
 		# ======================================
-		if datatype == 'scalar':
-			group=scalar_update(group,UID,value,verbose)
-
-		elif datatype == 'vector':
-			group=vector_update(group,UID,value,verbose)
-
-		elif datatype == 'array':
-			raise NotImplementedError('Arrays not implemented yet')
-
-		else:
-			raise TypeError('This is not a group supported by the E200_api')
-
+		uid_dset  = group['UID']
+		dat_dset  = group['dat']
+		refs_dset = group['refs']
+	
+		uids      = uid_dset.value
+		dat_refs  = dat_dset.value
+	
+		if verbose: print '\tAdding UID: {}, value: {}'.format(UID,value)
+	
 		# ======================================
-		# Write changes to file
+		# Validate it's a numpy array
 		# ======================================
+		value = _np.array(value)
+		
+		# Verify vector
+		# ndim = _np.ndim(value)
+		# if ndim != 2:
+		#         raise ValueError('Wrong dimensions for an array: {} dimensions'.format(ndim))
+	
+		# ======================================
+		# Find matches
+		# ======================================
+		uid_match = _np.in1d(uids,UID)
+		uid_match_total = _np.sum(uid_match)
+		uid_match_inds = _np.where(uid_match)[0]
+	
+		if uid_match_total == 0:
+			# ======================================
+			# Add new UID and value
+			# ======================================
+			uuid_val = str(_uuid.uuid1())
+			uuid_ref = refs_dset.create_dataset(name=uuid_val,data=value)
+	
+			uids_shape = uid_dset.shape
+			
+			uid_dset.resize((uids_shape[0]+1,))
+			dat_dset.resize((uids_shape[0]+1,))
+	
+			uid_dset[-1] = UID
+			dat_dset[-1] = uuid_ref.ref
+	
+		elif uid_match_total == 1:
+			# ======================================
+			# Delete old UID dataset
+			# ======================================
+			ind = uid_match_inds[0]
+			ref = dat_dset.value[ind]
+			ref_name = group.file[ref].name
+			split = ref_name.split('/')
+			ref_uuid = split[-1]
+			del refs_dset[ref_uuid]
+	
+			# ======================================
+			# Create new UUID dataset
+			# ======================================
+			uuid_val = str(_uuid.uuid1())
+			uuid_ref = refs_dset.create_dataset(name=uuid_val,data=value)
+	
+			# ======================================
+			# Replace reference
+			# ======================================
+			dat_dset[ind] = uuid_ref.ref
+		elif uid_match_total > 1:
+				# ======================================
+				# Too many matches, corruption?
+				# ======================================
+				raise LookupError('Multiple unique IDs exist. ({} uids with values: {})'.format(uid_match_total,uids[uid_match]))
+	
+			# ======================================
+			# Write changes to file
+			# ======================================
 		group.file.flush()
--- E200/E200_create_data.py
+++ E200/E200_create_data.py
@@ -1,3 +1,6 @@
+import uuid
+import h5py as h5
+
 __all__ = ['E200_create_data']
 def E200_create_data(group_parent,group_name,datatype):
 	# ======================================
@@ -14,7 +17,14 @@ def E200_create_data(group_parent,group_name,datatype):
 	elif datatype == 'vector':
 		group_child = _create_group_basic(group_parent,group_name,datatype)
 	elif datatype == 'array':
-		group_child = _create_group_basic(group_parent,group_name,datatype)
+		group_child = group_parent.create_group(group_name)
+		group_child.attrs.create('datatype',datatype)
+		group_child.create_dataset('UID',shape=(0,),maxshape=(None,))
+
+		ref_dtype = h5.special_dtype(ref=h5.Reference)
+		group_child.create_dataset('dat',shape=(0,),maxshape=(None,),dtype=ref_dtype)
+
+		group_child.create_group('refs')
 	else:
 		raise TypeError('Datatype "{}" is not supported'.format(datatype))
 
