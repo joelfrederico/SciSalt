1e317c92555f862501d388d660598101d64a87a1
Joel Frederico <joelfred@slac.stanford.edu>
2014-10-23 17:31:42 -0700

    Bug fixes

--- hardcode/setQS.py
+++ hardcode/setQS.py
@@ -18,9 +18,12 @@ class setQS(object):
 		self.energy_offset = energy_offset
 
 		
+	# ======================================
+	# QS1, QS2 class objects
+	# ======================================
 	def _get_QS1(self):
 		out = QS(
-				K1 = self._get_QS1_K1()
+				K1     = self._get_QS1_K1(),
 				length = QS1_length
 				)
 		return out
@@ -28,31 +31,45 @@ class setQS(object):
 		
 	def _get_QS2(self):
 		out = QS(
-				K1 = self._get_QS2_K1()
+				K1     = self._get_QS2_K1(),
 				length = QS2_length
 				)
 		return out
 	QS2 = property(_get_QS2)
 
+	# ======================================
+	# Get QS1/2's K1 value
+	# ======================================
 	def _get_QS1_K1(self):
 		QS1_BDES = self._get_QS1_BDES()
-		return sltr.BDES2K(bdes=QS1_BDES,quad_length = QS1_length,energy=self.energy):
+		return sltr.BDES2K(bdes=QS1_BDES,quad_length = QS1_length,energy=self.energy)
 
 	def _get_QS2_K1(self):
 		QS2_BDES = self._get_QS2_BDES()
-		return sltr.BDES2K(bdes=QS2_BDES,quad_length = QS2_length,energy=self.energy):
+		return sltr.BDES2K(bdes=QS2_BDES,quad_length = QS2_length,energy=self.energy)
 
-	def _get_QS1_BDES(self)
+	# ======================================
+	# Get QS1/2's BDES value
+	# ======================================
+	def _get_QS1_BDES(self):
 		QS1_BDES = (1+self.energy_offset/20.35)*261.72
 		return QS1_BDES
 
-	def _get_QS2_BDES(self)
+	def _get_QS2_BDES(self):
 		QS2_BDES = -(1+self.energy_offset/20.35)*167.95
 		return QS2_BDES
 
+	# ======================================
+	# Get energy in GeV
+	# ======================================
 	def _get_energy(self):
 		return self.energy_offset + energy0
 	def _set_energy(self,value):
 		self.energy_offset = value - energy0
 	energy = property(_get_energy,_set_energy)
 
+	# ======================================
+	# Get ELANEX's y motor position
+	# ======================================
+	def elanex_y_motor(self):
+		ymotor = 40 - 55*self.energy_offset/(self.energy_offset+20.35)
