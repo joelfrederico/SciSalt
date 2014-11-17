551b5fd868ceffa2b868b17cf6734e2c770aefa2
Joel Frederico <joelfred@slac.stanford.edu>
2014-11-17 13:00:18 -0800

    Updated with ability to use existing axis

--- hist.py
+++ hist.py
@@ -2,7 +2,7 @@ import numpy as _np
 import matplotlib.pyplot as _plt
 from addlabel import addlabel as _addlabel
 
-def hist(x, bins=10, labels=None, aspect="auto", plot=True, fig=None, range=None):
+def hist(x, bins=10, labels=None, aspect="auto", plot=True, ax=None, range=None):
 	"""Creates a 2D histogram of data."""
 
 	h,edge=_np.histogram(x,bins=bins,range=range)
@@ -11,7 +11,11 @@ def hist(x, bins=10, labels=None, aspect="auto", plot=True, fig=None, range=None
 	mids = mids[:-1]
 
 	if plot:
-		_plt.hist(x,bins=bins,range=range)
+		if ax is None:
+			_plt.hist(x,bins=bins,range=range)
+		else:
+			ax.hist(x,bins=bins,range=range)
+
 		if ( labels != None ):
 			_addlabel(labels[0],labels[1],labels[2])
 
