12eb246dbf2c3b4e8e03a469d6b256228adca707
Joel Frederico <joelfred@slac.stanford.edu>
2014-11-18 12:20:57 -0800

    Added code that accesses env vars

--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
 import logging, inspect
+import argparse
+import os
 
 class Keywords(object):
 	def __init__(self,**kwargs):
@@ -25,3 +27,45 @@ class IndentFormatter(logging.Formatter):
 		del rec.indent
 		#  del rec.function
 		return out
+
+# ================================
+# Access environment variables
+# ================================
+
+def checkvar(envvar):
+	try:
+		value=os.environ[envvar]
+	except:
+		value=None
+	# print(value)
+	return value
+
+# Email variable class
+class emailprefs:
+	# print(requested)
+	# print(value)
+	# raise argparse.ArgumentTypeError('hi')
+	"""Preferences for email notification."""
+	def __init__(self,requested=False,environvar=None,value=None):
+		self.requested = requested
+		self.environvar=environvar
+		if value==None:
+			self.value = checkvar(environvar)
+		else:
+			self.value = value
+		if self.value==None:
+			self.value = os.environ['PHYSICS_USER']
+			print 'Using current fphysics login: ' + self.value
+
+		# raise ValueError('problem')
+
+# Adds action to load email from $NOTIFY_EMAIL if possible
+class note_address(argparse.Action):
+	def __call__(self, parser, namespace, values, option_string=None):
+		# print getattr(namespace,self.dest).environvar
+		# print values
+		if values==None:
+			out=emailprefs(True,getattr(namespace,self.dest).environvar)
+		else:
+			out=emailprefs(True,getattr(namespace,self.dest).environvar,values)
+		setattr(namespace, self.dest, out)
