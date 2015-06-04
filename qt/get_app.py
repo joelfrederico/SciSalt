af0a04606be48c350da722b19f1dce5df95ee858
Joel Frederico <joelfred@slac.stanford.edu>
2015-06-03 23:03:30 -0700

    Made app a global variable so it won't be garbage-collected

--- qt/get_app.py
+++ qt/get_app.py
@@ -2,7 +2,8 @@ import sys
 from PyQt4 import QtGui
 
 
-def get_app(argv=[sys.argv[0]]):
+def get_app(argv=sys.argv):
+    global app
     app = QtGui.QApplication.instance()
     if not app:
         app = QtGui.QApplication(argv)
