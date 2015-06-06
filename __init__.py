6c7b7592a6e78275cb438a66e304ada98853ab19
Joel Frederico <joelfred@slac.stanford.edu>
2015-06-06 03:12:40 -0700

    More tools

--- __init__.py
+++ __init__.py
@@ -1,37 +1,37 @@
-from . import classes
-from . import facettools
-from . import graphics
-from . import hardcode
-from . import PWFA
-# from . import qt
+from . import classes                                    # NOQA
+from . import facettools                                 # NOQA
+from . import graphics                                   # NOQA
+from . import hardcode                                   # NOQA
+from . import PWFA                                       # NOQA
 
-from .BDES2K import *
-from .LinLsqFit import LinLsqFit
-from .addlabel import addlabel
-from .chisquare import chisquare
-from .create_group import create_group
-from .curve_fit_unscaled import curve_fit_unscaled
-from .derefdataset import *
-from .figure import figure
-from .findpinch import findpinch
-from .fitimageslice import fitimageslice
-from .gaussfit import _gauss
-from .gaussfit import _gaussvar
-from .gaussfit import gaussfit
-from .hist import hist
-from .hist2d import hist2d
-from .linspaceborders import linspaceborders
-from .linspacestep import linspacestep
-from .mylogger import *
-from .picklejar import picklejar
-from .plot_featured import plot_featured
-from .showfig import showfig
-from .NonUniformImage import *
-from .frexp10 import *
-from .pcolor_axes import pcolor_axes
-from .NonUniformImage_axes import NonUniformImage_axes
-
-from .h5drill import *
+from .BDES2K import *                                    # NOQA
+from .LinLsqFit import LinLsqFit                         # NOQA
+from .NonUniformImage import *                           # NOQA
+from .NonUniformImage_axes import NonUniformImage_axes   # NOQA
+from .addlabel import addlabel                           # NOQA
+from .chisquare import chisquare                         # NOQA
+from .create_group import create_group                   # NOQA
+from .curve_fit_unscaled import curve_fit_unscaled       # NOQA
+from .derefdataset import *                              # NOQA
+from .fft import fft
+from .figure import figure                               # NOQA
+from .fill_missing_timestamps import *                   # NOQA
+from .findpinch import findpinch                         # NOQA
+from .fitimageslice import fitimageslice                 # NOQA
+from .frexp10 import *                                   # NOQA
+from .gaussfit import _gauss                             # NOQA
+from .gaussfit import _gaussvar                          # NOQA
+from .gaussfit import gaussfit                           # NOQA
+from .h5drill import *                                   # NOQA
+from .hist import hist                                   # NOQA
+from .hist2d import hist2d                               # NOQA
+from .linspaceborders import linspaceborders             # NOQA
+from .linspacestep import linspacestep                   # NOQA
+from .mylogger import *                                  # NOQA
+from .pcolor_axes import pcolor_axes                     # NOQA
+from .picklejar import picklejar                         # NOQA
+from .plot_featured import plot_featured                 # NOQA
+from .showfig import showfig                             # NOQA
 
 
 class Error(Exception):
