0b967cb2228cf53034964e098104c9de77988f96
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-11 14:58:31 -0700

    Bumpversion

--- scisalt/__init__.py
+++ scisalt/__init__.py
@@ -1,3 +1,4 @@
+__version__ = '1.0.0'
 from . import classes                                    # noqa
 from . import facettools                                 # noqa
 from . import graphics                                   # noqa
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0rc0.dev1',
+    version='1.0.0',
 
     description='Tools to make scientific data analysis easier',
     long_description=long_description,
