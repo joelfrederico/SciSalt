3fb3a031dfba872824939565aea7020c282e276c
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-12 05:09:26 -0700

    Next fix

--- scisalt/qt/mplwidget.py
+++ scisalt/qt/mplwidget.py
@@ -17,20 +17,23 @@ import logging
 loggerlevel = logging.DEBUG
 logger = logging.getLogger(__name__)
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+import os as _os
+on_rtd = _os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    try:
+        _fromUtf8 = QtCore.QString.fromUtf8
+    except AttributeError:
+        def _fromUtf8(s):
+            return s
+    
+    try:
+        _encoding = QtGui.QApplication.UnicodeUTF8
+    
+        def _translate(context, text, disambig):
+            return QtGui.QApplication.translate(context, text, disambig, _encoding)
+    except AttributeError:
+        def _translate(context, text, disambig):
+            return QtGui.QApplication.translate(context, text, disambig)
 
 
 class Slider_and_Text(QtGui.QWidget):
