706694e6ab7391c996bbd6391838b98fbc5d12df
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-12 00:42:11 -0700

    h5py module was completely unnecessary and/or redundant.

--- docs/data/h5py.rst
+++
@@ -1,5 +0,0 @@
-h5py
-====
-
-.. automodule:: scisalt.h5py
-   :members:
--- scisalt/h5py/__init__.py
+++
@@ -1,7 +0,0 @@
-# Author: Joel Frederico
-"""
-The :mod:`scisalt.h5py` module provides ways of making it easier to parse :mod:`h5py` modules' into data, and manipulate that data.
-"""
-__all__ = ['convertH5ref', 'create_group', 'derefdataset', 'H5Drill']
-from .derefdataset import derefdataset
-from .h5drill import H5Drill
