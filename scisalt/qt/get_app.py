fc477f0a018c1254afcbe10c14e2736fb2a6f722
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-12 05:05:13 -0700

    PyQt4

--- scisalt/qt/get_app.py
+++ scisalt/qt/get_app.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt4 import QtGui
+import os as _os
+on_rtd = _os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    from PyQt4 import QtGui
 
 
 def get_app(argv=sys.argv):
