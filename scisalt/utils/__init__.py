9648c03c9935dd4b3f626234863093dd4b374255
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-30 17:06:19 -0700

    Keys utility function

--- scisalt/utils/__init__.py
+++ scisalt/utils/__init__.py
@@ -1,8 +1,10 @@
 __all__ = [
     'githubtunnel',
     'progressbar',
+    'printkeys',
     ]
 __all__.sort()
 
 from .githubtunnel import *  # noqa
 from .progressbar import progressbar
+from .keys import keys
---
+++ scisalt/utils/keys.py
@@ -0,0 +1,7 @@
+def keys(f):
+    """
+    .. versionadded:: 1.4
+
+    Returns an array of strings of the keys like Python 2 used to do.
+    """
+    return [key for key in f.keys()]
