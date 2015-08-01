4f79e9745955f81c2b79d4d70b4ee06a0790fa94
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-31 17:26:59 -0700

    Remove findpinch

--- scisalt/accelphys/__init__.py
+++ scisalt/accelphys/__init__.py
@@ -4,5 +4,4 @@ Calculates various useful quantities in accelerator physics.
 """
 __all__ = ['BDES2K', 'K2BDES', 'findpinch', 'fitimageslice']
 from .BDES2K import *  # noqa
-from .findpinch import findpinch
 from .fitimageslice import fitimageslice
