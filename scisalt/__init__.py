e2221c19c32b576498cbed80cf986b5f525252d1
Joel Frederico <joelfred@slac.stanford.edu>
2015-08-11 17:12:07 -0700

    Bump version: 1.4.4 → 1.4.5

--- scisalt/__init__.py
+++ scisalt/__init__.py
@@ -2,7 +2,7 @@
 """
 This project has slowly grown from frequently-used code snippets. The main objective is to create a collection of interconnected methods frequently needed to visualize and analyze data using `NumPy <http://www.numpy.org/>`_, `SciPy <http://www.scipy.org/>`_, `Matplotlib <http://matplotlib.org/>`_, and `PyQt4 <http://www.riverbankcomputing.com/software/pyqt/download>`_.
 """
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 from . import accelphys
 from . import facettools
 from . import logging
