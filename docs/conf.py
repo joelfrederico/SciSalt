8b3158af30dfab22a2989503f3f0e0322a543c42
Joel Frederico <joelfred@slac.stanford.edu>
2015-08-14 23:12:41 -0700

    Updated author

--- docs/conf.py
+++ docs/conf.py
@@ -263,7 +263,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'SciSalt.tex', 'SciSalt Documentation',
-   'Joel Frederico', 'manual'),
+   'Joel Frederico, SLAC National Accelerator Laboratory', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
