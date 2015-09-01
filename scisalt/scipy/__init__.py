2a2c42438cc42ff0b5a060cf534e8190ead8c9f8
Joel Frederico <joelfred@slac.stanford.edu>
2015-09-01 14:08:23 -0700

    Fix for cpython?

--- scisalt/scipy/__init__.py
+++ scisalt/scipy/__init__.py
@@ -9,8 +9,11 @@ from .fft import *                       # noqa
 from .fill_missing_timestamps import *   # noqa
 from .gaussfit import *                  # noqa
 
-import pyximport
-import numpy as _np
-pyximport.install(setup_args={"include_dirs": _np.get_include()})
+import os as _os
+_on_rtd = _os.environ.get('READTHEDOCS', None) == 'True'
+if not _on_rtd:
+    import pyximport
+    import numpy as _np
+    pyximport.install(setup_args={"include_dirs": _np.get_include()})
 
 from .hough_ellipse import hough_ellipse
